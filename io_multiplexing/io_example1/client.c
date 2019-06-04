45ceb39a9e73016f296f7f9cf21b628610e2471c
New91 <niu91@qq.com>
2019-06-04 18:43:11 +0800

    通过向量发送接收数据
    readv writev

--- io_multiplexing/io_example1/client.c
+++ io_multiplexing/io_example1/client.c
@@ -10,21 +10,73 @@
 #include <stdlib.h>
 #include <strings.h>
 
+#define PORT 8888
+#define V 1
+
+static struct iovec* vs = NULL;
+static struct iovec* vc = NULL;
+
 extern void sig_proccess(int signo);
 extern void sig_pipe(int signo);
 static int s;
-void sig_proccess_client(int signo)
+void sig_proccess(int signo)
 {
     printf("catch a exit signal\n");
-    _exit(0);
+
+#if V
+    if(vc)
+        free(vc);
+    if(vs)
+        free(vs);
+#endif
+    exit(0);
 }
 void sig_pipe(int signo)
 {
     printf("catch a pipe signal\n");
+
+#if V
+    if(vc)
+        free(vc);
+    if(vs)
+        free(vs);
+#endif
+    exit(0);
 }
 
-#define PORT 8888
 // 客户端的处理过程
+#if V
+void process_conn_client(int s)
+{
+    char buffer[30];
+    ssize_t size = 0;
+
+    struct iovec *v = (struct iovec*)malloc(3*sizeof(struct iovec));
+    vc = v; //挂接全局变量，便于释放
+    v[0].iov_base = buffer;
+    v[1].iov_base = buffer + 10;
+    v[2].iov_base = buffer + 20;
+    v[0].iov_len = v[1].iov_len = v[2].iov_len = 10;
+
+    int i = 0;
+    while(1)
+    {
+        // 从标准输入中读取数据放到缓冲区buffer中
+        size = read(0, v[0].iov_base, 10);
+        if(size > 0)
+            v[0].iov_len = size;
+        writev(s, v, 1);    // 发送给服务器
+        v[0].iov_len = v[1].iov_len = v[2].iov_len = 10;
+
+        // 从套接字中读取数据放到向量缓冲区中
+        size = readv(s, v, 3);                          // 从服务器读取数据
+        for(i=0;i<3;i++){
+            if(v[i].iov_len > 0)
+                write(1, v[i].iov_base, v[i].iov_len);//写到标准输出
+        }
+    }
+}
+#else
 void process_conn_client(int s)
 {
     ssize_t size = 0;
@@ -39,7 +91,7 @@ void process_conn_client(int s)
         }
     }
 }
-
+#endif
 int main(int argc, char* argv[])
 {
     struct sockaddr_in server_addr;
@@ -50,7 +102,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    signal(SIGINT, sig_proccess_client);
+    signal(SIGINT, sig_proccess);
     signal(SIGPIPE, sig_pipe);
 
     s = socket(AF_INET, SOCK_STREAM, 0);
--- io_multiplexing/io_example1/server.c
+++ io_multiplexing/io_example1/server.c
@@ -10,12 +10,35 @@
 #include <stdlib.h>
 #include <strings.h>
 
+#define V 1
+static struct iovec* vs = NULL;
+static struct iovec* vc = NULL;
+
 extern void sig_proccess(int signo);
 extern void sig_pipe(int signo);
 static int s;
 void sig_proccess(int signo)
 {
     printf("catch a exit signal\n");
+
+#if V
+    if(vc)
+        free(vc);
+    if(vs)
+        free(vs);
+#endif
+    exit(0);
+}
+void sig_pipe(int signo)
+{
+    printf("catch a pipe signal\n");
+
+#if V
+    if(vc)
+        free(vc);
+    if(vs)
+        free(vs);
+#endif
     exit(0);
 }
 
@@ -23,6 +46,41 @@ void sig_proccess(int signo)
 #define BACKLOG 2
 /* 服务端对客户端的处理
  * */
+
+#if 1
+
+void process_conn_server(int s)
+{
+    char buffer[30];
+    ssize_t size = 0;
+
+    struct iovec *v = (struct iovec*)malloc(3*sizeof(struct iovec));
+    vs = v; //挂接全局变量，便于释放
+    v[0].iov_base = buffer;
+    v[1].iov_base = buffer + 10;
+    v[2].iov_base = buffer + 20;
+    v[0].iov_len = v[1].iov_len = v[2].iov_len = 10;
+
+    while(1)
+    {
+        // 从套接字中读取数据放到向量缓冲区中
+        size = readv(s, v, 3);
+        if(size == 0)
+            return;
+
+        sprintf(v[0].iov_base, "%d ",size);
+        sprintf(v[1].iov_base, "bytes alt");
+        sprintf(v[2].iov_base, "ogether\n");
+        // 写入字符串长度
+        v[0].iov_len = strlen(v[0].iov_base);
+        v[1].iov_len = strlen(v[1].iov_base);
+        v[2].iov_len = strlen(v[1].iov_base);
+
+        writev(s, v, 3);
+    }
+}
+
+#else
 void process_conn_server(int s)
 {
     ssize_t size = 0;
@@ -37,6 +95,7 @@ void process_conn_server(int s)
         send(s, buffer, strlen(buffer)+1,0);//发送给客户端
     }
 }
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -49,7 +108,7 @@ int main(int argc, char* argv[])
 
 
     signal(SIGINT, sig_proccess);
-    signal(SIGPIPE, sig_proccess);
+    signal(SIGPIPE, sig_pipe);
 
     ss = socket(AF_INET, SOCK_STREAM, 0);
     if(ss < 0){
